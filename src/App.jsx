4cfdf42c7d14f4448ce4e838b2e3b1ef2a56a036
hluismoises <hluismoises@hotmail.com>
2025-01-26 14:00:38 -0600

    47 Best practice: Storing components in files & using a good project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,23 @@
-import reactImg from './assets/react-core-concepts.png';
-import { CORE_CONCEPTS } from './data.js';
-
-const reactDescriptions = ['Fundamentals', 'Crucial', 'Core'];
-
-function genRandomInt(max){
-  return Math.floor(Math.random() * max);
-}
-
-function Header(){
-  const description = reactDescriptions[genRandomInt(reactDescriptions.length)];
-  return(
-    <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  )
-}
-
-function CoreConcepts({image, title, description}){
-  return(
-    <li>
-    <img src={image} alt={title}/>
-    <h3>{title}</h3>
-    <p>{description}</p>
-    </li>
-  )
-}
-
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts 
-              image={CORE_CONCEPTS[0].image} 
-              title={CORE_CONCEPTS[0].title} 
+            <CoreConcepts
+              image={CORE_CONCEPTS[0].image}
+              title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
             />
-            <CoreConcepts {...CORE_CONCEPTS[1]}/>
-            <CoreConcepts {...CORE_CONCEPTS[2]}/>
-            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
